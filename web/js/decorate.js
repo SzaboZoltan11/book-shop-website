18bbe24faa89e1134481418f1e998bcfa31be698
Zoli <szabozola91@gmail.com>
2025-03-17 10:13:06 +0100

    box decorations fix

--- web/js/decorate.js
+++ web/js/decorate.js
@@ -1,36 +1,37 @@
-const boxes = []
-const boxCount = 20 
+(() => {
+    const boxes = []
+    const boxCount = 20
+    /** @type {HTMLElement} */
+    const container = document.getElementById('decoration')
 
-for (let i = 0; i < boxCount; i++) {
-
-    const x = Math.random() * window.innerWidth;
-    
-
-    const y = Math.random() * window.innerHeight;
-
-    const element = document.createElement('div');
-    element.style.position = 'absolute';
-    element.style.width = `${80 + Math.round(Math.random() * 170)}px`; 
-    element.style.height = element.style.width; 
-    element.style.backgroundColor = '#fff1'; 
-    element.style.top = `${Math.round(y)}px`;
-    element.style.left = `${Math.round(x)}px`;
-    element.style.transform = `rotate(45deg)`;
-    element.style.zIndex = `-1`; 
-    document.body.appendChild(element);
-    boxes.push({
-        element: element,
-        x: x,
-        y: y,
-        v: (Math.random() - 0.5) * 2, 
-    });
-}
-
-setInterval(() => {
-    for (const element of boxes) {
-        const x = element.x + Math.cos(performance.now() * 0.0003 * element.v) * 50;
-        const y = element.y + Math.sin(performance.now() * 0.0003 * element.v) * 50;
-        element.element.style.top = `${Math.round(y)}px`;
-        element.element.style.left = `${Math.round(x)}px`;
+    for (let i = 0; i < boxCount; i++) {
+        const width = 80 + Math.round(Math.random() * 170)
+        const x = (Math.random() * window.innerWidth) - (width / 2)
+        const y = (Math.random() * window.innerHeight) - (width / 2)
+        
+        const element = document.createElement('div')
+        element.style.position = 'absolute'
+        element.style.width = `${width}px`
+        element.style.height = `${width}px`
+        element.style.backgroundColor = '#fff1' 
+        element.style.top = `${Math.round(y)}px`
+        element.style.left = `${Math.round(x)}px`
+        element.style.transform = `rotate(45deg)`
+        container.appendChild(element)
+        boxes.push({
+            element: element,
+            x: x,
+            y: y,
+            v: (Math.random() - 0.5) * 2, 
+        })
     }
-}, 10);
+
+    setInterval(() => {
+        for (const element of boxes) {
+            const x = element.x + Math.cos(performance.now() * 0.0003 * element.v) * 50
+            const y = element.y + Math.sin(performance.now() * 0.0003 * element.v) * 50
+            element.element.style.top = `${Math.round(y)}px`
+            element.element.style.left = `${Math.round(x)}px`
+        }
+    }, 10)
+})()
