e2f0e79cb449c02b8f83c33c443a726efe9f7350
SzaboZoltan11 <szabozola91@gmail.com>
2025-05-12 22:02:45 +0200

    bugfix

--- web/index.php
+++ web/index.php
@@ -172,7 +172,7 @@ include '../src/check_token.php';
 </nav>
 
 <div id="cart-feedback" class="feedback">A könyv a kosárba került!</div>
-<div id="wishlist-feedback" class="feedback">A könyv a kosárba került!</div>
+<div id="wishlist-feedback" class="feedback">A könyv a kívánságlistába került!</div>
 
 
         <!-- MODAL -->
